f672f45344a8f3ca6dbe828a6b5b30a99d907e3e
Maksym Ryndia <ryndya.maxim@gmail.com>
2025-02-06 02:08:46 +0200

    style(website): add logo to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,9 @@ import ThemeSwitch from "./ThemeSwitch";
 import SearchButton from "./SearchButton";
 import { cn } from "./lib/utils";
 import { NavigationMenu } from "./navigation/NavigationMenu";
+import Image from "next/image";
+
+import Logo from "../public/static/images/logo.png";
 
 const Header = () => {
   return (
@@ -21,9 +24,17 @@ const Header = () => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 {typeof siteMetadata.headerTitle === "string" ? (
-                  <div className="hidden text-3xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                    <br />
+                  <div className="hidden sm:block">
+                    <div className="flex flex-row items-baseline gap-2">
+                      <Image
+                        src={Logo}
+                        alt="logo"
+                        width={40}
+                        height={40}
+                        className="aspect-square h-6 w-6"
+                      />
+                      <div className="text-3xl font-semibold">{siteMetadata.headerTitle}</div>
+                    </div>
                     <div className="text-sm font-normal text-muted-foreground sm:block">
                       {siteMetadata.description}
                     </div>
